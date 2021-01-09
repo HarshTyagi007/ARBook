187197b05e1c4d09c47aab5c2021c97d482e97ab
Harsh Tyagi <48311361+HarshTyagi007@users.noreply.github.com>
2021-01-09 11:42:31 +0530

    Files Added

---
+++ Assets/Resources/Scripts/AeroReset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AeroReset : MonoBehaviour
+{
+    Vector3 original;
+    // Start is called before the first frame update
+    void Start()
+    {
+        original = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        transform.localPosition = original; 
+    }
+}
---
+++ Assets/Resources/Scripts/ResetPosition.cs
@@ -0,0 +1,23 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    Vector3 originalpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalpos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        transform.localPosition = originalpos;
+    }
+}
